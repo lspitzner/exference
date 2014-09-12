a34cdce70c52bee85911b9511aa4c3b1ee752285
Lennart Spitzner <lsp@informatik.uni-kiel.de>
2014-09-12 17:53:49 +0200

    improve conversion Expression->HaskellSrc

--- src/Language/Haskell/Exference/ExpressionToHaskellSrc.hs
+++ src/Language/Haskell/Exference/ExpressionToHaskellSrc.hs
@@ -13,6 +13,10 @@ import Language.Haskell.Exts.Syntax
 
 
 
+-- TODO:
+-- 1) don't convert to some invented function "f"; use lambda instead
+-- 2) merge nested lambdas
+
 convert :: E.Expression -> Decl
 convert e = h e []
   where
@@ -24,35 +28,43 @@ convert e = h e []
         rhs = UnGuardedRhs $ convertExp rhsExp
 
 convertExp :: E.Expression -> Exp
-convertExp (E.ExpVar i) = Var $ UnQual $ Ident $ T.showVar i
-convertExp (E.ExpLit c) = Con $ UnQual $ Ident $ c
-convertExp (E.ExpLambda i e) =
-    Lambda noLoc [PVar $ Ident $ T.showVar i] (convertExp e)
-convertExp (E.ExpApply e1 e2)
+convertExp e = convertInternal 0 e
+
+parens :: Bool -> Exp -> Exp
+parens True e = Paren e
+parens False e = e
+
+convertInternal :: Int -> E.Expression -> Exp
+convertInternal _ (E.ExpVar i) = Var $ UnQual $ Ident $ T.showVar i
+convertInternal _ (E.ExpLit c) = Con $ UnQual $ Ident $ c
+convertInternal p (E.ExpLambda i e) = parens (p>=1) $
+    Lambda noLoc [PVar $ Ident $ T.showVar i] (convertInternal 0 e)
+convertInternal p (E.ExpApply e1 e2)
   | (E.ExpApply op e0) <- e1,
     (E.ExpLit ('(':opId1)) <- op,
     opId <- takeWhile (/=')') opId1 =
       if opId == ","
-        then Tuple Unboxed [convertExp e0, convertExp e2]
-        else InfixApp (convertExp e0)
+        then Tuple Boxed [convertInternal 0 e0, convertInternal 0 e2]
+        else parens (p>=2) $ InfixApp (convertInternal 1 e0)
                         (QVarOp $ UnQual $ Symbol $ opId)
-                        (convertExp e2)
-  | otherwise = App (convertExp e1) (Paren $ convertExp e2)
-convertExp (E.ExpHole i) = Var $ UnQual $ Ident $ "_"++T.showVar i
-convertExp (E.ExpLet i bindE inE) =
+                        (convertInternal 2 e2)
+  | otherwise = parens (p>=3)
+              $ App (convertInternal 2 e1) (convertInternal 3 e2)
+convertInternal _ (E.ExpHole i) = Var $ UnQual $ Ident $ "_"++T.showVar i
+convertInternal p (E.ExpLet i bindE inE) =
   let convBind = PatBind noLoc
                    (PVar $ Ident $ T.showVar i)
-                   (UnGuardedRhs $ convertExp bindE)
+                   (UnGuardedRhs $ convertInternal 0 bindE)
                    (BDecls [])
-  in mergeLet convBind (convertExp inE)
-convertExp (E.ExpLetMatch n ids bindE inE) =
+  in parens (p>=2) $ mergeLet convBind (convertInternal 0 inE)
+convertInternal p (E.ExpLetMatch n ids bindE inE) =
   let convBind = PatBind noLoc
                    (PParen $ PApp (UnQual $ Ident $ n)
                                       (map (PVar . Ident . T.showVar)
                                            ids))
-                   (UnGuardedRhs $ convertExp bindE)
+                   (UnGuardedRhs $ convertInternal 0 bindE)
                    (BDecls [])
-  in mergeLet convBind (convertExp inE)
+  in parens (p>=2) $ mergeLet convBind (convertInternal 0 inE)
 
 mergeLet :: Decl -> Exp -> Exp
 mergeLet convBind (Let (BDecls otherBinds) finalIn)
