4448407dbef47366f95547aa2e89a167479706ad
Lennart Spitzner <lsp@informatik.uni-kiel.de>
2014-09-19 14:02:39 +0200

    reduce memory usage (at least when not debugging)

--- src/Language/Haskell/Exference/Internal/Exference.hs
+++ src/Language/Haskell/Exference/Internal/Exference.hs
@@ -36,6 +36,7 @@ import Control.Applicative ( (<$>), (<*>) )
 import Data.List ( partition, sortBy, groupBy )
 import Data.Ord ( comparing )
 import Data.Function ( on )
+import Data.Bool (bool)
 
 -- import Data.DeriveTH
 import Debug.Hood.Observe
@@ -72,6 +73,9 @@ type ExferenceOutputElement = (Expression, ExferenceStats)
 
 type RatedStates = Q.MaxPQueue Float State
 
+__debug :: Bool
+__debug = False
+
 findExpressions :: ExferenceInput
                 -> [ExferenceOutputElement]
 findExpressions (ExferenceInput rawCType funcs staticContext allowUnused) =
@@ -93,7 +97,7 @@ findExpressions (ExferenceInput rawCType funcs staticContext allowUnused) =
       ""
     helper :: Int -> RatedStates -> [(Int,Float,Expression)]
     helper n states
-      | Q.null states || n > 65535 = []
+      | Q.null states || n > 32767 = []
       | ((_,s), restStates) <- Q.deleteFindMax states =
         let (potentialSolutions, futures) = partition (null.state_goals) 
                                                       (stateStep s)
@@ -196,7 +200,7 @@ stateStep2 s
           vEnd
           (state_maxTVarId s)
           (state_depth s + factorFunctionGoalTransform)
-          (Just s)
+          (bool Nothing (Just s) __debug)
           "function goal transform"
     byProvided = do
       (provId, provT, provPs) <- scopeGetAllBindings (state_providedScopes s) scopeId
@@ -263,7 +267,7 @@ stateStep2 s
               (maximum $ state_maxTVarId s
                        : map largestId dependencies)
               (state_depth s + depthModNoMatch) -- constant penalty for wild-guessing..
-              (Just s)
+              (bool Nothing (Just s) __debug)
               ("randomly trying to apply function " ++ show coreExp)
         Just substs -> do
           let contxt = state_context s
@@ -299,7 +303,7 @@ stateStep2 s
                      : largestSubstsId substs
                      : map largestId dependencies)
             (state_depth s + depthModMatch)
-            (Just s)
+            (bool Nothing (Just s) __debug)
             (reasonPart ++ ", because " ++ substsTxt ++ " and because " ++ provableTxt)
 
 addScopePatternMatch :: Int -> ScopeId -> [VarPBinding] -> State -> State
@@ -333,7 +337,7 @@ addScopePatternMatch vid sid bindings state = foldr helper state bindings where
             state_expression = fillExprHole vid expr $ state_expression s,
             state_nextVarId = vEnd,
             state_maxTVarId = maximum (state_maxTVarId s:map largestId newProvTypes),
-            state_previousState = Just s,
+            state_previousState = bool Nothing (Just s) __debug,
             state_lastStepReason = "pattern matching on " ++ showVar v
           }
 
