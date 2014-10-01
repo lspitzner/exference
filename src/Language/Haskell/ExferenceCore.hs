2ba7b2165fcc3c336d56566b9a6f1237b9d2c10b
Lennart Spitzner <lsp@informatik.uni-kiel.de>
2014-10-02 00:00:04 +0200

    parallelize algorithm (provided as separate functions) (->0.6.3.0)

--- src/Language/Haskell/ExferenceCore.hs
+++ src/Language/Haskell/ExferenceCore.hs
@@ -2,6 +2,9 @@ module Language.Haskell.ExferenceCore
   ( findExpressions
   , findExpressionsChunked
   , findExpressionsWithStats
+  , findExpressionsPar
+  , findExpressionsChunkedPar
+  , findExpressionsWithStatsPar
   , E.ExferenceHeuristicsConfig (..)
   , E.ExferenceInput (..)
   , E.ExferenceOutputElement
@@ -12,6 +15,9 @@ where
 
 import qualified Language.Haskell.Exference.Internal.Exference as E
 import qualified Language.Haskell.Exference.ExferenceStats as S
+import qualified ListT
+import Control.Monad ( join )
+import Data.Monoid ( mempty )
 
 
 findExpressions :: E.ExferenceInput -> [E.ExferenceOutputElement]
@@ -24,3 +30,25 @@ findExpressionsChunked = map snd . E.findExpressions
 findExpressionsWithStats :: E.ExferenceInput
                          -> [(S.BindingUsages, [E.ExferenceOutputElement])]
 findExpressionsWithStats = E.findExpressions
+
+findExpressionsPar :: E.ExferenceInput
+                   -> (ListT.ListT IO E.ExferenceOutputElement
+                       -> IO a)
+                   -> IO a
+findExpressionsPar input reducer =
+  E.findExpressionsPar input $
+    reducer . (>>= foldr ListT.cons mempty) . fmap snd
+
+findExpressionsChunkedPar :: E.ExferenceInput
+                          -> (ListT.ListT IO [E.ExferenceOutputElement]
+                              -> IO a)
+                          -> IO a
+findExpressionsChunkedPar input reducer =
+  E.findExpressionsPar input $ reducer . fmap snd
+
+findExpressionsWithStatsPar
+  :: E.ExferenceInput
+  -> (   ListT.ListT IO E.ExferenceChunkElement
+      -> IO a)
+  -> IO a
+findExpressionsWithStatsPar = E.findExpressionsPar
